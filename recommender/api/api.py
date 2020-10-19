7978bb04d80c2691fe41b87b6fda0f02f68190f4
MrPekar98 <martin.pekar@outlook.dk>
2020-10-19 09:24:54 +0200

    Update

--- recommender/api/api.py
+++ recommender/api/api.py
@@ -1,16 +1,49 @@
 from flask import Flask, request, jsonify
 import json
+import pandas as pd
 import db
+import recommend as rec
 
 app = Flask(__name__)
 
+@app.route('/')
+def main_page():
+    return "Send POST to '/data' to upload wine deals.\nSend GET to '/data' to read wine deals recommendation."
+
+# TODO: First check if database is not empty.
 @app.route('/data', methods = ['GET'])
 def read_recommendation():
-    return "GET request"
+    if db.empty():
+        return "No recommendations to make."
+
+    data = {
+        "WineDeals": __wine_maps(db.get_ranked_wines())
+    }
+
+    return data
+
+# Returns list of maps of wine deals in pandas dataframe.
+def __wine_maps(frame):
+    res = []
+
+    for index, row in frame.iterrows():
+        res.append({
+            "name": row['name'],
+            "lwin": row['lwin'],
+            "rank": row['rank']
+        })
+
+    return res
 
 @app.route('/data', methods = ['POST'])
 def write_data():
-    return "POST request"
+    parsed = json.loads(request.data)
+    
+    # TODO: Create a pandas dataframe of parsed JSON wines and it into recommend.py.
+    # TODO: Insert output into db.py.
+
+    database = db.wine_db()
+    database.add_wines(rec.recommend(None))
 
 if (__name__ == "__main__"):
     app.run()
--- recommender/api/db.py
+++ recommender/api/db.py
@@ -1,5 +1,5 @@
 import sqlite3
-import pandas as pd
+from pandas import DataFrame
 
 # Wine database class.
 class wine_db:
@@ -40,7 +40,8 @@ class wine_db:
         if (self.connection == None):
             raise Exception("Wine database is closed.")
 
-        t = tuple(name, lwin, rank)
+        t = (name, lwin, rank)
+        print(self.__sql_str_insert(t))
 
         self.reopen()
         self.connection.execute("INSERT INTO wines (name, lwin, rank) VALUES (" + self.__sql_str_insert(t) + ")")
@@ -62,15 +63,17 @@ class wine_db:
         self.close()
 
     # Returns full string of SQL insertion values.
-    def __sql_str_insert(wine):
+    def __sql_str_insert(wine_field_tuple):
         res = ""
 
-        for attribute in wine:
+        for attribute in wine_field_tuple:
             if type(attribute) is str:
-                res = res + "'" + attribute + "'"
+                res = res + "'" + attribute + "',"
 
             else:
-                res = res + attribute
+                res = res + attribute + ","
+
+        return res[:len(res) - 1]
 
     # Returns sorted pandas dataframe.
     def get_ranked_wines(self):
@@ -96,3 +99,11 @@ class wine_db:
         self.connection.execute("DELETE FROM wines")
         self.connection.commit()
         self.close()
+
+database = wine_db()
+database.add_wine("Wine 1", 123, 2)
+database.add_wine("Wine 2", 7654, 1)
+database.add_wine("Wine 3", 273, 3)
+
+df = database.get_ranked_wines()
+print(df)
