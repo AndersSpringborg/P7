420524de744568b6fa8fdb6bbf4da199d23aa82a
kristiantg <kristiantgregersen@gmail.com>
2020-11-14 17:43:54 +0100

    Added routing for wine info
    Co-Authored-By: elaurs <34130623+elaurs@users.noreply.github.com>

--- worse-frontend/src/App.tsx
+++ worse-frontend/src/App.tsx
@@ -3,13 +3,22 @@ import { Router, Switch, Route } from "react-router-dom";
 import history from "./history";
 import "./App.css";
 import WineOfferList from "./components/wine-offer-list";
+import OfferTable from "./components/offer-table/offer-table";
+import WineInfo from "./components/offer-table/wine-offer-page/wine-offer-info";
 
 function App() {
   return (
     <Router history={history}>
       <Switch>
         <Route exact path="/">
-          <WineOfferList />
+          <WineOfferList>
+            <OfferTable />
+          </WineOfferList>
+        </Route>
+        <Route exact path="/wineOffer/:id">
+          <WineOfferList>
+            <WineInfo></WineInfo>
+          </WineOfferList>
         </Route>
       </Switch>
     </Router>
