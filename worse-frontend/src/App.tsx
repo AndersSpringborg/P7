3baef7feedba4f376365860fa496ffb517faf56f
Elisabeth Laursen <enl@rarewine.dk>
2020-11-15 14:30:04 +0100

    Added routing for transaction page and file system cleanup
    Clean up implies changing the structure and naming of files

--- worse-frontend/src/App.tsx
+++ worse-frontend/src/App.tsx
@@ -2,23 +2,29 @@ import React from "react";
 import { Router, Switch, Route } from "react-router-dom";
 import history from "./history";
 import "./App.css";
-import WineOfferList from "./components/wine-offer-list";
-import OfferTable from "./components/offer-table/offer-table";
-import WineInfo from "./components/offer-info/offer-info";
+import WorstLayout from "./components/worst-layout";
+import OfferTable from "./components/offers/offer-table/offer-table";
+import WineInfo from "./components/offers/offer-info/offer-info";
+import TransactionTable from "./components/transactions/transaction-table/transaction-table";
 
 function App() {
   return (
     <Router history={history}>
       <Switch>
         <Route exact path="/">
-          <WineOfferList>
+          <WorstLayout>
             <OfferTable />
-          </WineOfferList>
+          </WorstLayout>
         </Route>
         <Route exact path="/wineOffer/:id">
-          <WineOfferList>
-            <WineInfo></WineInfo>
-          </WineOfferList>
+          <WorstLayout>
+            <WineInfo />
+          </WorstLayout>
+        </Route>
+        <Route exact path="/transactions">
+          <WorstLayout>
+            <TransactionTable />
+          </WorstLayout>
         </Route>
       </Switch>
     </Router>
