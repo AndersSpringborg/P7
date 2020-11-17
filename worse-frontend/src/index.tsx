89a6b971198a72a9fece60c556c8a9fcbdc107ec
kristiantg <kristiantgregersen@gmail.com>
2020-11-17 23:43:28 +0100

    FARVER
    btw har commited min json package

--- worse-frontend/src/index.tsx
+++ worse-frontend/src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "antd/dist/antd.css";
-import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
