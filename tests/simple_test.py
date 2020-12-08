54f2345fd5aec3bece49b1a7454ff0cdc55d176d
MrPekar98 <martin.pekar@outlook.dk>
2020-12-08 12:01:14 +0100

    Starts services as new threads/

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,7 +1,24 @@
+import pytest
 import test
 import requests
+import subprocess as proc
+import threading
+import time
+
+
+def start_services():
+    api = threading.Thread(target = proc.run, args = (['python3', '../API/api.py', 'local'],))
+    db = threading.Thread(target = proc.run, args = (['python3', '../database/apiDB.py'],))
+    recommender = threading.Thread(target = proc.run, args = (['python3', '../recommender/webapp.py'],))
+    api.start()
+    db.start()
+    recommender.start()
+    time.sleep(3)
+
 
 def test_components():
+    start_services()
+
     requests.post('http://127.0.0.1:49500/global_prices', headers = test.headers, json = test.global_prices)
     requests.post('http://127.0.0.1:49500/data/transactions', headers = test.headers, data = test.transactions)
     requests.post('http://127.0.0.1:49500/data/wine_deals', headers = test.headers, json = test.offers)
@@ -10,5 +27,3 @@ def test_components():
 
     test.test_get_recommendation()
     test.test_get_wine()
-
-test_components()
