e377288aec48cea90fb0074029a58c78a91eeccb
MrPekar98 <martin.pekar@outlook.dk>
2020-12-10 11:03:32 +0100

    Terminates processes.

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -4,20 +4,23 @@ import requests
 import subprocess as proc
 import threading
 import time
-
+import subprocess
+import os
 
 def start_services():
-    api = threading.Thread(target = proc.run, args = (['python3', '../API/api.py', 'local'],))
-    db = threading.Thread(target = proc.run, args = (['python3', '../database/apiDB.py'],))
-    recommender = threading.Thread(target = proc.run, args = (['python3', '../recommender/webapp.py'],))
-    api.start()
-    db.start()
-    recommender.start()
+    api_pid = subprocess.Popen(["python3", "../API/api.py", "local"]).pid
+    db_pid = subprocess.Popen(["python3", "../database/apiDB.py"]).pid
+    rec_pid = subprocess.Popen(["python3", "../recommender/webapp.py"]).pid
     time.sleep(3)
 
+    return [api_pid, db_pid, rec_pid]
+
+def stop_services(pids):
+    for pid in pids:
+        os.kill(pid, 9)
 
 def test_components():
-    start_services()
+    pids = start_services()
 
     requests.post('http://127.0.0.1:49500/global_prices', headers = test.headers, json = test.global_prices)
     requests.post('http://127.0.0.1:49500/data/wine_deals', headers = test.headers, json = test.offers)
@@ -27,3 +30,5 @@ def test_components():
 
     test.test_get_recommendation()
     test.test_get_wine()
+
+    stop_services(pids)
